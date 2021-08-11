ce0abdd443a8c42614190bc5bc8f7272dafd8ca5
Iqbal Bin Mohamad Zainuddin <iqbalz.dev@gmail.com>
2021-08-11 22:11:57 +0800

    work on associative array

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-    
+    // 5-Separate logic from presentation code start here
     // Comment code below to test array test code
     // Also can be uncommented to include header element in the page while display array elements
     $greeting = "Hello, World!";
+    // 5-Separate logic from presentation code end here
     
     // 6-Array test code start here
     // This is how to create array variable
@@ -12,12 +13,46 @@
         'Amy'
     ];
 
+    // Add value to array
+    $names[] = 'Kendrick';
+
     // Filter through array
     // foreach ($names as $name) {
     //     echo $name . ', ';
     // }
     // 6-Array test code end here
 
+    // 7-Associative array start here
+    // Create array that have key at value assigned to it
+    $person = [
+        'age' => 22,
+        'hair' => 'brown',
+        'career' => 'Web Developer'
+    ];
+
+    // Push new key and value to the array
+    $person['name'] = 'Hadid';
+
+    // use unset() to remove key and value pair from an array
+    unset($person['age']);
+
+    // echo can print string, but cannot print array
+    // use var_dump() instead
+    // var_dump(what to dump here)
+
+    // Using pre tag can format the var_dump() output
+    // <pre>
+    // var_dump()
+    // </pre>
+    // code does not stop after var_dump(), next code will still run
+
+    // die() will end the code when it is called, next code won't be run
+    // die()
+
+    // 7-Associative array end here
+
+    // 5-Separate logic from presentation code start here
     // Comment code below to test array test code
     // Uncomment this code below when you want to display array elements in index view page
     require 'index.view.php';
+    // 5-Separate logic from presentation code end here
